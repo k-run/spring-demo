36cae282571c0cdcf35f1c19ba5007a9abbcd95e
karanchapaneri <karan.chapaneri@gmail.com>
2019-02-17 14:52:24 +0530

    Message for homepage

--- src/main/java/com/example/apidemo/ApidemoApplication.java
+++ src/main/java/com/example/apidemo/ApidemoApplication.java
@@ -5,10 +5,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Controller
 public class ApidemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello, World!. This is my first spring app..";
+    }
 
 	public static void main(String[] args) {
 
